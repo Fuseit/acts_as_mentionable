20221a31fa24407f7a5cc3be4eff8ea0177958c9
Nazar Vinnychuk <ux.isco@gmail.com>
2018-11-27 19:46:52 +0200

    chore Get rid of Gemfile.lock dependency

--- spec/acts_as_mentionable_spec.rb
+++ spec/acts_as_mentionable_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe ActsAsMentionable do
   it 'has a version number' do
-    expect(ActsAsMentionable::VERSION).not_to be nil
+    expect(ActsAsMentionable::VERSION).to match /^\d+\.\d+\.\d+$/
   end
 end
